e900ccb6e8bebebd793a18ad797edc72aac22d41
Aspekt112 <Aspekt112@gmail.com>
2016-11-05 00:04:21 +0300

    Fourth excercise done!

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,27 @@
+# -*- coding: utf-8 -*-
+
 import json
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        path = str(input('Введите путь к JSON файлу: '))
+    except ValueError or TypeError:
+        path = None
+    if path is None:
+        print('Некорректные данные. См. пример использования.')
+    else:
+        json_content = load_data(path)
+        pretty_print_json(json_content)
